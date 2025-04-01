0d55977e25daeffe33e9a9624eebfc7406d49ee0
mikk-c <mcos@itu.dk>
2025-04-01 10:18:43 +0200

    Updated documentation

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src/'))
+
 project = 'Node Vector Distance'
 copyright = '2025, Michele Coscia'
 author = 'Michele Coscia'
